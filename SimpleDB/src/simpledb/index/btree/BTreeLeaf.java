f7223ba48dff95bbd5c5e617cef005ced72b3892
steven <codeSteven@163.com>
2017-12-14 23:55:57 +0800

    完整索引 添加hash索引

--- SimpleDB/src/simpledb/index/btree/BTreeLeaf.java
+++ SimpleDB/src/simpledb/index/btree/BTreeLeaf.java
@@ -86,6 +86,67 @@ public class BTreeLeaf {
 				return;
 			}
 	}
+
+	/**
+	 * Inserts a new leaf record having the specified dataRID
+	 * and the previously-specified search key.
+	 * If the record does not fit in the page, then
+	 * the page splits and the method returns the
+	 * directory entry for the new page;
+	 * otherwise, the method returns null.
+	 * If all of the records in the page have the same dataval,
+	 * then the block does not split; instead, all but one of the
+	 * records are placed into an overflow block.
+	 * @param datarid the dataRID value of the new record
+	 * @return the directory entry of the newly-split page, if one exists.
+	 */
+	public DirEntry insert(RID datarid) {
+		// bug fix:  If the page has an overflow page
+		// and the searchkey of the new record would be lowest in its page,
+		// we need to first move the entire contents of that page to a new block
+		// and then insert the new record in the now-empty current page.
+		if (contents.getFlag() >= 0 && contents.getDataVal(0).compareTo(searchkey) > 0) {
+			Constant firstval = contents.getDataVal(0);
+			Block newblk = contents.split(0, contents.getFlag());
+			currentslot = 0;
+			contents.setFlag(-1);
+			contents.insertLeaf(currentslot, searchkey, datarid);
+			return new DirEntry(firstval, newblk.number());
+		}
+
+		currentslot++;
+		contents.insertLeaf(currentslot, searchkey, datarid);
+		if (!contents.isFull())
+			return null;
+		// else page is full, so split it
+		Constant firstkey = contents.getDataVal(0);
+		Constant lastkey  = contents.getDataVal(contents.getNumRecs()-1);
+		if (lastkey.equals(firstkey)) {
+			// create an overflow block to hold all but the first record
+			Block newblk = contents.split(1, contents.getFlag());
+			contents.setFlag(newblk.number());
+			return null;
+		}
+		else {
+			int splitpos = contents.getNumRecs() / 2;
+			Constant splitkey = contents.getDataVal(splitpos);
+			if (splitkey.equals(firstkey)) {
+				// move right, looking for the next key
+				while (contents.getDataVal(splitpos).equals(splitkey))
+					splitpos++;
+				splitkey = contents.getDataVal(splitpos);
+			}
+			else {
+				// move left, looking for first entry having that key
+				while (contents.getDataVal(splitpos-1).equals(splitkey))
+					splitpos--;
+			}
+			Block newblk = contents.split(splitpos, -1);
+			return new DirEntry(splitkey, newblk.number());
+		}
+	}
+
+
 	private boolean tryOverflow() {
 		Constant firstkey = contents.getDataVal(0);
 		int flag = contents.getFlag();
